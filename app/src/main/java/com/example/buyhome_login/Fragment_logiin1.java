c46a6b98597f0119f6d067db1500c8e0ad6bf3b2
uu <betty8398@gmail.com>
2020-12-09 21:07:27 +0800

    更新邊框顏色/可以用firebase會員登入
    透過EditText的內容即時更新邊框顏色
    user登入可以和firebase已存在的會員列表比對

--- app/src/main/java/com/example/buyhome_login/Fragment_logiin1.java
+++ app/src/main/java/com/example/buyhome_login/Fragment_logiin1.java
@@ -3,14 +3,20 @@ package com.example.buyhome_login;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -18,7 +24,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class Fragment_logiin1 extends Fragment {
     private Button button_signIn;
@@ -26,6 +38,12 @@ public class Fragment_logiin1 extends Fragment {
     private GoogleSignInClient mGoogleSignInClient;
     private final int RC_SIGN_IN=100;//當作確認有登入的碼
     private String TAG="main";
+    private EditText editText_email,editText_password;
+    private TextView textView_toRegister,textView_toForgotPassword;
+    private View email_frame_red;
+    private String email,password;
+    private FirebaseAuth authControl;
+    private FirebaseUser currentUser;
 
     public Fragment_logiin1() {
         // Required empty public constructor
@@ -45,6 +63,31 @@ public class Fragment_logiin1 extends Fragment {
         //取得元件
         button_signIn=view.findViewById(R.id.button_signIn);
         button_gLogin=view.findViewById(R.id.button_gLogin);
+        editText_email=view.findViewById(R.id.editText_email);
+        editText_password=view.findViewById(R.id.editText_password);
+        textView_toRegister=view.findViewById(R.id.textView_toRegister);
+        textView_toForgotPassword=view.findViewById(R.id.textView_toForgotPassword);
+        email_frame_red = view.findViewById(R.id.email_frame_red);
+
+        //FirebaseAuth 實體
+        authControl = FirebaseAuth.getInstance();
+        Log.d(TAG, "authControl = " + authControl);
+
+        //按下Register 轉向 Register頁面
+        textView_toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment_logiin1_to_fragment_login_regester);
+            }
+        });
+
+        //按下忘記密碼 前往忘記密碼頁面
+        textView_toForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Sorry we can't help you~", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //TODO:轉接到成功登入畫面
         button_signIn.setOnClickListener(new View.OnClickListener() {
@@ -57,8 +100,6 @@ public class Fragment_logiin1 extends Fragment {
                 }
             }
         });
-
-
         //TODO:按下google登入按鈕 轉接到Google登入API
         button_gLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +108,70 @@ public class Fragment_logiin1 extends Fragment {
             }
         });
 
+        //TODO:填寫email及時顯示反應
+        editText_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d(TAG, "afterTextChanged: editable = "+s);
+                String p = s.toString();
+                if(p.indexOf("@")>-1){
+                    Log.d(TAG, "email has @");
+                    email_frame_red.setVisibility(View.INVISIBLE);
+                }else {
+                    email_frame_red.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        //TODO:按下LOGIN 傳送資料到RealTime Database 比對帳號密碼
+        button_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = editText_email.getText().toString();
+                password = editText_password.getText().toString();
+                //檢查有沒有user在線上 有的話登出 (一次只能一個人登入firebase)
+                currentUser = authControl.getCurrentUser();
+                if (currentUser != null) {
+                    authControl.signOut();
+                }
+                //登入firebase 參數帶入帳號密碼
+                authControl.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() { //設立登入完成的監聽器
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "login ok");
+                                    FirebaseUser user = authControl.getCurrentUser();
+                                    DisplayUser(user);
+                                } else {
+                                    Log.d(TAG, "login fail");
+                                    Toast.makeText(getActivity(), "LoginFail", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
         return view;
+    }
+
+    private void DisplayUser(FirebaseUser user) {
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        String UID = user.getUid();
+        Log.d(TAG, "DisplayUser: name = "+name);
+        Log.d(TAG, "DisplayUser: email = "+email);
+        Log.d(TAG, "DisplayUser: UID = " + UID);
     }
 
 
@@ -125,8 +229,6 @@ public class Fragment_logiin1 extends Fragment {
             // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
-            //TODO:目前都是登入失敗
-
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
@@ -137,15 +239,16 @@ public class Fragment_logiin1 extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        //TODO:
-        // 確認有沒有登入過的帳號 如果有 GoogleSignInAccount物件!=null
+        //TODO:確認有沒有登入過的帳號 如果有 GoogleSignInAccount物件!=null
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getActivity());
         updateUI(account);
-
     }
     private void updateUI(GoogleSignInAccount account){
         if(account==null){//如果抓取不到使用者最近登入 物件 則跳轉到登入畫面
             //TODO:要轉跳的畫面  (這個應該做在購物車或用戶資料登入)
+            Toast.makeText(getActivity(), "登入失敗", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getActivity(), "登入成功", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/buyhome_login/Fragment_login_regester.java
+++ app/src/main/java/com/example/buyhome_login/Fragment_login_regester.java
@@ -3,10 +3,18 @@ package com.example.buyhome_login;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +31,11 @@ public class Fragment_login_regester extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button button_singUP;
+    private EditText editText_name,editText_Remail,editText_Rpassword,editText_Rpassword2;
+    private TextView textView_toSingIn;
+    private View view_name, view_email, view_Rpassword2, view_Rpassword;
+    private String TAG="main";
 
     public Fragment_login_regester() {
         // Required empty public constructor
@@ -59,6 +72,89 @@ public class Fragment_login_regester extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_regester, container, false);
+        View view =inflater.inflate(R.layout.fragment_login_regester, container, false);
+
+        button_singUP=view.findViewById(R.id.button_singUP);
+        textView_toSingIn=view.findViewById(R.id.textView_toSingIn);
+
+        editText_name=view.findViewById(R.id.editText_name);
+        editText_Remail=view.findViewById(R.id.editText_Remail);
+        editText_Rpassword=view.findViewById(R.id.editText_Rpassword);
+        editText_Rpassword2=view.findViewById(R.id.editText_Rpassword2);
+
+        view_name =view.findViewById(R.id.name_frame_red);
+        view_email =view.findViewById(R.id.email_fram_red);
+        view_Rpassword =view.findViewById(R.id.password_fram_red);
+        view_Rpassword2 =view.findViewById(R.id. password2_fram_red);
+
+        //TODO:監聽editText是否有值 如果點選後沒填寫過 就換成紅色
+        editText_name.addTextChangedListener(new MyEditTextFrame(view_name));
+        editText_Remail.addTextChangedListener(new MyEditTextFrame(view_email));
+        editText_Rpassword.addTextChangedListener(new MyEditTextFrame(view_Rpassword));
+        editText_Rpassword2.addTextChangedListener(new MyEditTextFrame(view_Rpassword2));
+
+
+        //當按下註冊按鈕
+        button_singUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText_name.length()==0 ){
+                    Toast.makeText(getActivity(), "請輸入名字", Toast.LENGTH_SHORT).show();
+                    view_name.setVisibility(View.VISIBLE);
+                }else if(editText_Remail.length()==0){
+                    Toast.makeText(getActivity(), "請輸入email", Toast.LENGTH_SHORT).show();
+                    view_email.setVisibility(View.VISIBLE);
+                }else if(editText_Rpassword.length()==0){
+                    Toast.makeText(getActivity(), "請輸入密碼", Toast.LENGTH_SHORT).show();
+                    view_Rpassword.setVisibility(View.VISIBLE);
+                }
+                //else if(!editText_Rpassword.equals(editText_Rpassword2)){
+                else if(!editText_Rpassword.getText().toString().equals(editText_Rpassword2.getText().toString())){
+                    Log.d(TAG, "onClick: "+editText_Rpassword.getText().toString());
+                    Toast.makeText(getActivity(), "密碼確認不符合", Toast.LENGTH_SHORT).show();
+                    view_Rpassword2.setVisibility(View.VISIBLE);
+                    //TODO:做成紅框框
+                }else {
+                    String name = editText_name.getText().toString();
+                    String email = editText_Remail.getText().toString();
+                    String password = editText_Rpassword.getText().toString();
+                    view_Rpassword2.setVisibility(View.INVISIBLE);
+                    view_email.setVisibility(View.INVISIBLE);
+                    view_name.setVisibility(View.INVISIBLE);
+                    //TODO:將資料傳到雲端
+                }
+            }
+        });
+
+        //按下sing in 前往登入頁面
+        textView_toSingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragment_login_regester_to_fragment_logiin1);
+            }
+        });
+        return view;
+    }
+
+    private class MyEditTextFrame implements TextWatcher {
+        View viewfram;
+        public MyEditTextFrame(View v) {
+            viewfram=v;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(s.length()!=0){
+                viewfram.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 }
