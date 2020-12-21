f25b601f9325ce8d14c0823fd98240189f8c55a6
Lucien-Hsu <meteor831@gmail.com>
2020-12-21 10:21:42 +0800

    fix:結帳完會回到首頁
    #issue f1_001

--- app/src/main/java/com/example/buyhome_login/fragment_cart/DoPay.java
+++ app/src/main/java/com/example/buyhome_login/fragment_cart/DoPay.java
@@ -3,6 +3,7 @@ package com.example.buyhome_login.fragment_cart;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -208,7 +209,13 @@ public class DoPay extends Fragment {
                 new AlertDialog.Builder(context)
                         .setTitle("Warning")
                         .setMessage(getString(R.string.gateway_replace_name_example))
-                        .setPositiveButton("OK", null)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //TODO 回到首頁
+                                requireActivity().finish();
+                            }
+                        })
                         .create()
                         .show();
             }
